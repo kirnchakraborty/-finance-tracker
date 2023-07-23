74407d3fa1abfe94837ad1afc8751465f4c4f18b
Kirn Chakraborty <kirnchakraborty@yahoo.co.uk>
2023-07-23 11:54:24 +0100

    Added Font Awesome, Form and Show Price

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
    root "welcome#index"
-
+   get 'my_portfolio', to: 'users#my_portfolio'
+   get 'search_stock', to: 'stocks#search'
+ 
    devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
  end
